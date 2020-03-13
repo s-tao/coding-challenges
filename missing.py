4ac68cbca7fc8f5d489e4fc1c257e78f18f94771
Sarah Tao <satao.94@gmail.com>
2020-03-13 17:23:31 +0000

    missing num complete

---
+++ missing.py
@@ -0,0 +1,26 @@
+"""Given a list of numbers 1...max_num, find which one is missing in a list."""
+
+
+def missing_number(nums, max_num):
+    """Given a list of numbers 1...max_num, find which one is missing.
+
+    *nums*: list of numbers 1..[max_num]; exactly one digit will be missing.
+    *max_num*: Largest potential number in list
+
+    >>> missing_number([7, 3, 2, 4, 5, 6, 1, 9, 10], 10)
+    8
+    
+    """
+
+    total = sum(range(max_num+1))
+    missing_total = sum(nums)
+
+    missing_num = total - missing_total
+
+    return missing_num
+    
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASS. NICELY DONE!\n")
