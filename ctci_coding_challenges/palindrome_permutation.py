244349acd1a57b13eca37836e6812e269bb2f2ba
Sarah Tao <satao.94@gmail.com>
2020-05-17 00:18:40 -0700

    palindrome permutation complete

--- ctci_coding_challenges/palindrome_permutation.py
+++ ctci_coding_challenges/palindrome_permutation.py
@@ -1,6 +1,6 @@
 """Given a string, write a function to check if it is a permutation of a 
 palindrome. A palindrome is a word or phrase that is the same forwards and 
-backwards. A permutation is a rearrangement of letters.The palindrome does not 
+backwards. A permutation is a rearrangement of letters. The palindrome does not 
 need to be limited to just dictionary words.
 
 Tact Coa
@@ -8,6 +8,29 @@ Output: True (permutations: "taco cat". "atco cta". etc.)
 """
 
 def permutation_is_palindrome(word):
-    pass
+    
+    char_dict = {}
 
+    for char in word:
+        if char != " ":
+            char_dict[char.lower()] = char_dict.get(char.lower(), 0) + 1
+
+    counter = 0
+
+    for count in char_dict:
+        if char_dict[count] % 2 != 0:
+            counter += 1
+
+        if counter > 1:
+            return False
+    
+    return True
+
+# testcases
 print(permutation_is_palindrome('Tact Coa')) # True 
+print(permutation_is_palindrome('Not a palindrome')) # False 
+print(permutation_is_palindrome('aaa')) # True 
+print(permutation_is_palindrome('aaab')) # False 
+print(permutation_is_palindrome('aaaab')) # True 
+print(permutation_is_palindrome('aaaabc')) # False 
+print(permutation_is_palindrome('aaaabcc')) # True 
